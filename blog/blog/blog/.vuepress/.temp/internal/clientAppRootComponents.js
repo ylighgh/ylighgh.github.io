b48ba4c63dca1346a8f2a934a1a063ee7a566983
ylighgh <475254161@qq.com>
2022-04-07 09:45:58 +0800

    test

--- blog/blog/blog/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from '/home/ylighgh/workspace/blog/node_modules/@vuepress/plugin-pwa-popup/lib/client/components/PwaPopupWrapper.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- blog/blog/blog/.vuepress/.temp/pages/post/java/2022/04/07/测试/index.html.vue
+++
@@ -1,3 +0,0 @@
-<template><h2 id="just-for-test" tabindex="-1"><a class="header-anchor" href="#just-for-test" aria-hidden="true">#</a> Just for test</h2>
-<h3 id="test" tabindex="-1"><a class="header-anchor" href="#test" aria-hidden="true">#</a> test</h3>
-</template>
